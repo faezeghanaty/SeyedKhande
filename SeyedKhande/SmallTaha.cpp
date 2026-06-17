d9696e9e3eabe68babeeea414882892d5c214499
faezeghanaty <faezeghanaty@gmail.com>
2026-06-17 15:17:12 +0330

    adding smalltaha class

---
+++ SeyedKhande/SmallTaha.cpp
@@ -0,0 +1,66 @@
+#include "SmallTaha.h"
+SmallTaha::SmallTaha(int energy) : Hero("SmallTaha",3,"Healer",500,4,"Hame dar jay khod!in mantaqe alode ast")
+{
+
+}
+Hero::targetorteamtype SmallTaha::playerteam(int x)
+{
+	switch (x)
+	{
+	case 1: return lowest; break;
+	case 2: return optional; break;
+	case 3: return lowest; break;
+	default: return nobody; break;
+	}
+}
+
+Hero::targetorteamtype SmallTaha::playertarget(int x)
+{
+	switch (x)
+	{
+	case 1: return optional; break;
+	case 2: return nobody; break;
+	case 3: return nobody; break;
+	default: return nobody; break;
+
+	}
+}
+
+void SmallTaha::ability1(Hero::context& c, vector<Effects>& list)
+{
+	c.team[0]->heal(20);
+	bool f = false;
+	for (auto x : list)
+	{
+		if (x.getclassname() == "DrWhite" && x == name && x.isactive())
+		{
+			int p = x.applypercent(30);
+			c.team[c.targetindex]->reducingHP(p);
+			f = true;
+		}
+	}
+	if (!f)
+	{
+		c.team[c.targetindex]->reducingHP(30);
+	}
+	reducingenergy(2);
+}
+
+void SmallTaha::ability2(Hero::context& c, vector<Effects>& list)
+{
+	c.team[c.teamindex]->heal(40);
+	Effects e("SmallTaha", c.team[c.teamindex]->getname(), 1, 40);
+	list.emplace_back(e);
+	reducingenergy(4);
+}
+
+bool SmallTaha::special(Hero::context& c, vector<Effects>& list)
+{
+	if (rounds == activespecialpower)
+	{
+		return 0;
+	}
+	c.team[0]->heal(200);
+	reducingenergy(4);
+	return 1;
+}
---
+++ SeyedKhande/SmallTaha.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Hero.h"
+class SmallTaha : public Hero
+{
+	SmallTaha(int energy);
+	virtual Hero::targetorteamtype playerteam(int x) override;
+	virtual Hero::targetorteamtype playertarget(int x) override;
+	virtual void ability1(Hero::context& c, vector <Effects>& list) override;
+	virtual void ability2(Hero::context& c, vector <Effects>& list) override;
+	virtual bool special(Hero::context& c, vector <Effects>& list) override;
+};
+
